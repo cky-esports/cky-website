3586eb13f00dca4e64c045be496c47afaa1b59ef
Brian Le <developer.cky@gmail.com>
2019-03-14 20:34:02 +0000

    Add comment to where the import returns undefined

--- packages/client/src/components/shared/ContentWrapper/ContentWrapper.js
+++ packages/client/src/components/shared/ContentWrapper/ContentWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { theme } from 'client-misc';
 
-console.log(theme);
+console.log(theme); // Returns undefined
 
 const StyledContentWrapper = styled.div`
   /*padding: 0px ${theme.spacing.unit * 3}px;
